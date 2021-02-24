170cf4610744652e0408fd5ab70e3efd6f630198
iamzjt-front-end <iamzjt@foxmail.com>
2021-02-24 15:49:47 +0800

    scss引入测试

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <div id="app">
-
+    Hello World!
+    <i class="iconfont icon-settings"></i>
   </div>
 </template>
 
 <style lang="scss" scoped>
+@import "~assets/css/variable.scss";
 
+#app {
+  color: $color-theme;
+}
 </style>
