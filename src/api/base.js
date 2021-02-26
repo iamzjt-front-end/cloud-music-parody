843197c0bd3813d822dceb00d8d052eaed9dc688
iamzjt-front-end <iamzjt@foxmail.com>
2021-02-26 13:04:55 +0800

    api 模块化，base-url管理，index-同意api出口，login-登陆模块

---
+++ src/api/base.js
@@ -0,0 +1,9 @@
+/**
+ * 接口域名的管理
+ * 可以自行定义多个
+ */
+const base = {
+    baseURL: 'https://api.lk-lmusic.top',
+}
+
+export default base;
---
+++ src/api/login.js
@@ -0,0 +1,21 @@
+/**
+ * login模块接口列表
+ */
+import base from './base'; // 导入接口域名列表
+import axios from '@/axios'; // 导入http中创建的axios实例
+// import qs from 'qs'; // 根据需求是否导入qs模块
+
+const login = {
+    // 密码登录
+    passwordLogin (params) {
+        return axios.get(`${base.baseURL}/login/cellphone`, {
+            params: params
+        });
+    },
+    // post提交
+    // articleDetail1 (params) {
+    //     return axios.post(`${base.baseURL}/accesstoken`, qs.stringify(params));
+    // }
+}
+
+export default login;
