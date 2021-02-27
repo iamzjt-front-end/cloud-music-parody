168911a8d1d2d2b8aaa292ba91cdc19fe284111c
iamzjt-front-end <iamzjt@foxmail.com>
2021-02-27 12:43:35 +0800

    封装TabBar

---
+++ src/components/TabBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="tab-bar">
+    <van-tabbar v-model="active" active-color="#ee0a24" inactive-color="#000">
+      <van-tabbar-item name="found">
+        <span>发现</span>
+        <template>
+          <i class="iconfont icon-found"></i>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item name="mine">
+        <span>我的</span>
+        <template>
+          <i class="iconfont icon-mine"></i>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item name="cloud-village">
+        <span>云村</span>
+        <template>
+          <i class="iconfont icon-cloud-village"></i>
+        </template>
+      </van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBar",
+  data() {
+    return {
+      active: 'found',
+    };
+  },
+}
+</script>
+
+<style scoped>
+.tab-bar {
+
+}
+</style>
