93f630193409a85117a2fe3f77fae701e7aa6b71
iamzjt-front-end <iamzjt@foxmail.com>
2021-03-02 00:39:06 +0800

    码云部署修改baseURL

--- src/api/base.js
+++ src/api/base.js
@@ -5,7 +5,8 @@
 const base = {
     // baseURL: 'https://api.lk-lmusic.top',
     // baseURL: 'http://lk-music.cn1.utools.club',
-    baseURL: ' http://localhost:8080/api',
+    // baseURL: ' http://localhost:8080/api',
+    baseURL: ' https://zjt1326002840.gitee.io/api',
 }
 
 export default base;
