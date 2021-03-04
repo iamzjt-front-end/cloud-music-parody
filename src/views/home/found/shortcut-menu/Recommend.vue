caa1b92cdbd7110ec7d8e65058a01676e9a794e9
iamzjt-front-end <iamzjt@foxmail.com>
2021-03-04 23:10:57 +0800

    Song组件基本完成

--- src/views/home/found/shortcut-menu/Recommend.vue
+++ src/views/home/found/shortcut-menu/Recommend.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="recommend">
+    <!-- 顶栏 -->
     <top-bar>
-      <i class="iconfont icon-back" slot="left"></i>
+      <i class="iconfont icon-back" slot="left" @click="backToHome"></i>
       <h1 slot="center">每日推荐</h1>
       <i class="iconfont icon-more" slot="right"></i>
     </top-bar>
+    <!-- 主图 -->
     <div class="main-img" :style="{backgroundImage:`url(${mainImgUrl})`}">
       <p class="calendar">
         <i class="day">04</i>
@@ -14,21 +16,33 @@
         今日运势：吉
       </p>
     </div>
+    <!-- 播放全部 -->
     <div class="play-all">
       <i class="iconfont icon-play-all" slot="left"></i>
       <span class="play-text">播放全部</span>
       <p class="play-length">{{ '(' + perDayRecList.length + ')' }}</p>
+    </div>
+    <!-- 推荐歌曲曲目 -->
+    <div class="recommend-song">
+      <song v-for="(item, index) in this.perDayRecList" :key="index">
+        <img :src="item.al.picUrl" slot="front-cover">
+        <h1 slot="song-name">{{ item.name }}</h1>
+        <p slot="song-author">{{ item.ar[0].name }} - {{ item.al.name }}</p>
+        <i class="iconfont icon-more" slot="operate"></i>
+      </song>
     </div>
   </div>
 </template>
 
 <script>
 import TopBar from "@/components/TopBar";
+import Song from "@/components/Song";
 
 export default {
-  name: "recommend",
+  name: "Recommend",
   components: {
-    TopBar
+    TopBar,
+    Song,
   },
   data() {
     return {
@@ -37,6 +51,7 @@ export default {
     }
   },
   methods: {
+    // 每日推荐获取
     perDayRecGet() {
       let that = this;
       this.$api.found.perDayRecQry().then(res => {
@@ -45,6 +60,10 @@ export default {
         let index = Math.floor(33 * Math.random());
         that.mainImgUrl = res.data.data.dailySongs[index].al.picUrl;
       })
+    },
+    // 返回主页
+    backToHome() {
+      this.$router.push({path: '/found'});
     }
   },
   mounted() {
@@ -110,7 +129,7 @@ export default {
   .play-all {
     width: 100%;
     height: 3.4rem;
-    background-color: #fff;
+    background-color: pink;
     display: flex;
 
     .icon-play-all {
@@ -137,6 +156,12 @@ export default {
       font-size: 0.8rem;
       padding: 1.35rem 0 0 0.2rem;
     }
+  }
+
+  .recommend-song {
+    width: 100%;
+    height: calc(100vh - 28vh - 3.4rem);
+    background-color: #fff;
   }
 }
 </style>
