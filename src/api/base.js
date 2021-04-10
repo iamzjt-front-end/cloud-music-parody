efae1a02548cea580ad74d9ad6344f9864a57b44
iamzjt-front-end <iamzjt@foxmail.com>
2021-04-10 13:10:37 +0800

    服务端跨域设置

--- src/api/base.js
+++ src/api/base.js
@@ -3,8 +3,9 @@
  * 可以自行定义多个
  */
 const base = {
-    baseURL: 'https://api.lk-lmusic.top', // 远程服务
-    // baseURL: ' http://localhost:8080/api', // 本地启服务 - 跨域代理
+    // baseURL: 'https://api.lk-lmusic.top', // 远程服务
+    // baseURL: 'http://localhost:8080/api', // 本地启服务 - 跨域代理
+    baseURL: 'https://zjt1326002840.gitee.io', // 服务端
 }
 
 export default base;
