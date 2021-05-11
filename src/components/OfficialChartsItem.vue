c05dbb6fa6849185dcefefc71259a85a3c324eb1
iamzjt-front-end <iamzjt@foxmail.com>
2021-05-11 23:19:20 +0800

    封装官方榜 Item 组件

---
+++ src/components/OfficialChartsItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="official-charts-item">
+    <div class="left">
+
+    </div>
+    <div class="right">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OfficialChartsItem",
+  props: ['item'],
+
+}
+</script>
+
+<style lang="scss" scoped>
+#official-charts-item {
+  width: 100%;
+  height: 8rem;
+  margin-top: 0.8rem;
+  background-color: pink;
+  border-radius: 0.6rem;
+}
+</style>
