0819fe5253f4e54938236f07e5103c6aeacf8e2e
yanxiangxiang <1443796228@qq.com>
2021-05-13 09:39:03 +0800

    切到远程服务

--- src/api/base.js
+++ src/api/base.js
@@ -3,8 +3,8 @@
  * 可以自行定义多个
  */
 const base = {
-    // baseURL: 'https://api.lk-lmusic.top', // 远程服务
-    baseURL: 'http://localhost:8080/api', // 本地启服务 - 跨域代理
+    baseURL: 'https://api.lk-lmusic.top', // 远程服务
+    // baseURL: 'http://localhost:8080/api', // 本地启服务 - 跨域代理
 }
 
 export default base;
