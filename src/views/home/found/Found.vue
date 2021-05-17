03fe951a9473ee1ee2f56a0113d2d59ce43ee57d
iamzjt-front-end <iamzjt@foxmail.com>
2021-05-18 01:32:12 +0800

    调整轮播图移动速度

--- src/views/home/found/Found.vue
+++ src/views/home/found/Found.vue
@@ -151,12 +151,13 @@ export default {
           autoplay: true,
           easing: 'transition-timing-function',
           interval: '5000', // 距离下一次播放的间隔
+          speed: '1000', // 切换 Page 动画的默认时长
         },
         useTransition: true,
-        momentum: false, // 当使用 slide 时，这个值需要设置为 false，用来避免惯性动画带来的快速滚动时的闪烁的问题和快速滑动时一次滚动多页的问题。
-        bounce: false, // bounce 值需要设置为 false，否则会在循环衔接的时候出现闪烁。
+        momentum: false, // 当使用 slide 时，这个值需要设置为 false，用来避免惯性动画带来的快速滚动时的闪烁的问题和快速滑动时一次滚动多页的问题
+        bounce: false, // bounce 值需要设置为 false，否则会在循环衔接的时候出现闪烁
         stopPropagation: true,
-        probeType: 2, // 如果你想通过监听 slideWillChange 事件，在用户拖动 slide 时，实时获取到 slide 的 PageIndex 的改变，需要设置 probeType 值为 2 或者 3。
+        probeType: 2, // 如果你想通过监听 slideWillChange 事件，在用户拖动 slide 时，实时获取到 slide 的 PageIndex 的改变，需要设置 probeType 值为 2 或者 3
       })
       this.swiperSlider.on('slideWillChange', (page) => {
         that.currentPageIndex = page.pageX;
