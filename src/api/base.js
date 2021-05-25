3af5a8947de43376f78bd80476ba49f5733565db
iamzjt-front-end <iamzjt@foxmail.com>
2021-05-25 11:28:54 +0800

    添加远程服务2

--- src/api/base.js
+++ src/api/base.js
@@ -3,7 +3,8 @@
  * 可以自行定义多个
  */
 const base = {
-     //baseURL: 'https://api.lk-lmusic.top', // 远程服务
+     //baseURL: 'https://api.lk-lmusic.top', // 远程服务1
+     //baseURL: 'http://121.41.169.239:3000/', // 远程服务2
      // baseURL: 'http://localhost:8080/api', // 本地启服务 - 跨域代理
      baseURL: '/api',
 }
