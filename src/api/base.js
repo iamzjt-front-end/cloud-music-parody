67552b85a3b0426fc09a3a5987ecf0dc058d57fb
iamzjt-front-end <iamzjt@foxmail.com>
2021-05-27 12:22:48 +0800

    服务器配置

--- src/api/base.js
+++ src/api/base.js
@@ -4,9 +4,9 @@
  */
 const base = {
      //baseURL: 'https://api.lk-lmusic.top', // 远程服务1
-     // baseURL: 'http://121.41.169.239:3000', // 远程服务2
+     baseURL: 'http://121.41.169.239:3000', // 远程服务2
      // baseURL: 'http://localhost:8080/api', // 本地启服务 - 跨域代理
-     baseURL: '/api',
+     // baseURL: '/api',
 }
 
 export default base;
