df80c8efcb7cb6fc3926a4cdd1285982faa46808
iamzjt-front-end <iamzjt@foxmail.com>
2022-04-21 13:52:16 +0800

    ..

--- src/api/base.js
+++ src/api/base.js
@@ -3,11 +3,10 @@
  * 可以自行定义多个
  */
 const base = {
-     //baseURL: 'https://api.lk-lmusic.top', // 远程服务1
-     baseURL: 'https://api.iamzjt.online', // 远程服务2
-     // baseURL: 'http://localhost:3000', // 本地服务
-     // baseURL: 'http://localhost:8080/api', // 本地启服务 - 跨域代理
-     // baseURL: '/api',
-}
+  baseURL: "https://121.41.169.239:3000", // 远程服务
+  // baseURL: "http://localhost:3000", // 本地服务
+  // baseURL: "http://localhost:8080/api", // 本地启服务 - 跨域代理
+  // baseURL: "/api",
+};
 
 export default base;
--- vue.config.js
+++ vue.config.js
@@ -1,31 +1,29 @@
-const path = require('path') // 引入path模块
+const path = require("path"); // 引入path模块
 function resolve(dir) {
-    return path.join(__dirname, dir) // path.join(__dirname)设置绝对路径
+  return path.join(__dirname, dir); // path.join(__dirname)设置绝对路径
 }
 module.exports = {
-    chainWebpack: config => {
-        config.resolve.alias
-            // set第一个参数：设置的别名，第二个参数：设置的路径
-            .set('@', resolve('src'))
-            .set('assets', resolve('src/assets'))
-            .set('views', resolve('src/views'))
-            .set('components', resolve('src/components'))
-            .set('plugins', resolve('src/plugins'))
+  chainWebpack: (config) => {
+    config.resolve.alias
+      // set第一个参数：设置的别名，第二个参数：设置的路径
+      .set("@", resolve("src"))
+      .set("assets", resolve("src/assets"))
+      .set("views", resolve("src/views"))
+      .set("components", resolve("src/components"))
+      .set("plugins", resolve("src/plugins"));
+  },
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000", // 接口域名 - 本地服务
+        secure: true, // 如果是https接口，需要配置这个参数
+        changeOrigin: true, // 是否跨域
+        pathRewrite: {
+          "^/api": "",
+        },
+      },
     },
-    // devServer: {
-    //     proxy: {
-    //         '/api': {
-    //             // target: 'https://api.lk-lmusic.top', // 接口域名
-    //             // target: 'http://121.41.169.239:3000', // 接口域名 - 远程服务
-    //             target: 'http://localhost:3000', // 接口域名 - 本地服务
-    //             secure: true, // 如果是https接口，需要配置这个参数
-    //             changeOrigin: true, // 是否跨域
-    //             pathRewrite: {
-    //                 '^/api': ''
-    //             }
-    //         }
-    //     }
-    // },
-    publicPath: './',
-    productionSourceMap: false,
-}
+  },
+  publicPath: "./",
+  productionSourceMap: false,
+};
